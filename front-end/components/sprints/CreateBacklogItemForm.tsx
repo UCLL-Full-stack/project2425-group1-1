9904b649fc0d26fb46350311a2036833ef00cb95
tanna-1 <tan.nayir@student.ucll.be>
2024-11-02 09:51:54 +0100

    Added backlog item creation form to sprints page

---
+++ front-end/components/sprints/CreateBacklogItemForm.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Sprint, BacklogItem, ErrorResponse } from "@types";
+import SprintService from "@services/SprintService";
+import BacklogItemService from "@services/BacklogItemService";
+import styles from "@styles/form.module.css";
+
+type Props = {
+  sprint: Sprint;
+  backlogAddedCallback: (new_item: BacklogItem) => void;
+};
+
+const CreateBacklogItemForm: React.FC<Props> = ({ sprint, backlogAddedCallback }: Props) => {
+  const [successMessage, setSuccessMessage] = useState<string>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    const form = new FormData(e.currentTarget);
+    e.preventDefault();
+
+    setSuccessMessage("");
+    setErrorMessage("");
+
+    let resp = await BacklogItemService.createBacklogItem({
+      title: form.get("title") as string,
+      description: form.get("description") as string,
+      priority: Number(form.get("priority")),
+      estimatedHours: Number(form.get("estimatedHours")),
+    });
+
+    if (!resp.ok) {
+      const error = await resp.json() as ErrorResponse;
+      setErrorMessage(error.message);
+      return;
+    }
+
+    if (sprint.id == undefined)
+      return;
+
+    const backlog_item = await resp.json() as BacklogItem;
+    resp = await SprintService.addBacklogItemsToSprint(String(sprint.id), [backlog_item.id ?? -1]);
+    if (!resp.ok) {
+      const error = await resp.json() as ErrorResponse;
+      setErrorMessage(error.message);
+      return;
+    }
+
+    setSuccessMessage("Successfully added!");
+    backlogAddedCallback(backlog_item);
+  };
+
+  return (
+    <>
+      {successMessage && (<div className="alert alert-success" role="alert">
+        {successMessage}
+      </div>
+      )}
+      {errorMessage && (<div className="alert alert-danger" role="alert">
+        {errorMessage}
+      </div>
+      )}
+      {sprint && (
+        <>
+          <form onSubmit={submitHandler} className={styles.backlogItemForm}>
+            <label htmlFor="formTitle">Title</label>
+            <input className="form-control" id="formTitle" required name="title" />
+            <label htmlFor="formDescription">Description</label>
+            <input className="form-control" id="formDescription" required name="description" />
+            <label htmlFor="formPriority">Priority</label>
+            <input className="form-control" id="formPriority" required type="number" name="priority" />
+            <label htmlFor="formEstimatedHours">Estimated Hours</label>
+            <input className="form-control" id="formEstimatedHours" required type="number" name="estimatedHours" />
+            <button type="submit" className="btn btn-primary">Create</button>
+          </form>
+        </>
+      )}
+    </>
+  );
+};
+
+export default CreateBacklogItemForm;
--- front-end/pages/sprints/index.tsx
+++ front-end/pages/sprints/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Header from '@components/header';
 import { useEffect, useState } from 'react';
-import { Sprint } from '@types';
+import { BacklogItem, Sprint } from '@types';
 import SprintService from '@services/SprintService';
 import SprintOverviewTable from '@components/sprints/SprintOverviewTable';
 import SprintDetails from '@components/sprints/SprintDetails';
+import CreateBacklogItemForm from '@components/sprints/CreateBacklogItemForm';
 
 const Sprints: React.FC = () => {
     const [sprints, setSprints] = useState<Array<Sprint>>();
@@ -13,6 +14,12 @@ const Sprints: React.FC = () => {
     const getSprints = async () => {
         const resp = await SprintService.getAllSprints();
         setSprints(await resp.json());
+    };
+
+    const backlogAddedCallback = async (new_item: BacklogItem) => {
+        // I suppose it's bad practice to mutate state so we'll fetch a new sprint
+        const resp = await SprintService.getSprintById(String(selectedSprint?.id ?? -1));
+        setSelectedSprint(await resp.json());
     };
 
     useEffect(() => { getSprints() }, []);
@@ -31,10 +38,14 @@ const Sprints: React.FC = () => {
                         sprints && (<SprintOverviewTable sprints={sprints} selectSprint={setSelectedSprint} />)
                     }
 
-                    {selectedSprint
-                        && (<h2>Details of {selectedSprint.name}</h2>)}
-                    {selectedSprint
-                        && (<SprintDetails sprint={selectedSprint} />)}
+                    {selectedSprint && (
+                        <>
+                            <h2>Add new backlog item to {selectedSprint.name}</h2>
+                            <CreateBacklogItemForm sprint={selectedSprint} backlogAddedCallback={backlogAddedCallback} />
+                            <h2>Details of {selectedSprint.name}</h2>
+                            <SprintDetails sprint={selectedSprint} />
+                        </>
+                    )}
                 </section>
             </main>
         </>
